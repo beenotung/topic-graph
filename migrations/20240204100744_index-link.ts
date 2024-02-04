711feea4eca2461806353967dab9c87983f61476
Beeno Tung <aabbcc1241@yahoo.com.hk>
2024-02-04 18:37:47 +0800

    patch: deduplicate links

--- migrations/20240204100744_index-link.ts
+++ migrations/20240204100744_index-link.ts
@@ -1,6 +1,22 @@
 import type { Knex } from 'knex'
 
 export async function up(knex: Knex): Promise<void> {
+  let rows = await knex.raw(/* sql */ `
+  select id, from_topic_id, to_topic_id from link
+  group by from_topic_id, to_topic_id
+  having count(id) > 1
+  `)
+  for (let row of rows) {
+    await knex.raw(
+      /* sql */ `
+    delete from link
+    where from_topic_id = :from_topic_id
+      and to_topic_id = :to_topic_id
+      and id <> :id
+    `,
+      row,
+    )
+  }
   await knex.schema.alterTable('link', table => {
     table.unique(['from_topic_id', 'to_topic_id'])
   })
